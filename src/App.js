0ad7437591f654736c81c03efddfb347ca2cf932
issal30 <jrvskaren@gmail.com>
2022-07-22 15:31:39 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="content">
+      <img
+        className="img"
+        src="https://statik.tempo.co/data/2019/09/06/id_870368/870368_720.jpg"
+      />
+      <div className="grid">
+      <img className="img-component" src="https://aasnova.org/wp-content/uploads/2021/09/360-cm-la-silla-cc.jpg"/>
+      <img className="img-component" src="https://aasnova.org/wp-content/uploads/2021/09/360-cm-la-silla-cc.jpg"/>
+      <img className="img-component" src="https://aasnova.org/wp-content/uploads/2021/09/360-cm-la-silla-cc.jpg"/>
+      <img className="img-component" src="https://asset.kompas.com/crops/_eRqssG2UqFM6fcVOTkkR89ngXQ=/0x0:1800x1200/780x390/data/photo/2022/06/04/629b19acaba3c.jpg"/>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
