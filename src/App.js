12c3728726fb04ac5e470a17e66e4c54f8fd3574
issal30 <jrvskaren@gmail.com>
2022-08-06 16:53:12 +0700

    update stateless to stateful

--- src/App.js
+++ src/App.js
@@ -1,20 +1,82 @@
+import { Component } from "react";
 import "./App.css";
-function App() {
-  return (
-    <div className="content">
-      <img
-        className="img"
-        src="https://statik.tempo.co/data/2019/09/06/id_870368/870368_720.jpg"
-      />
-      <div className="grid">
-      <img className="img-component" src="https://aasnova.org/wp-content/uploads/2021/09/360-cm-la-silla-cc.jpg"/>
-      <img className="img-component" src="https://aasnova.org/wp-content/uploads/2021/09/360-cm-la-silla-cc.jpg"/>
-      <img className="img-component" src="https://aasnova.org/wp-content/uploads/2021/09/360-cm-la-silla-cc.jpg"/>
-      <img className="img-component" src="https://asset.kompas.com/crops/_eRqssG2UqFM6fcVOTkkR89ngXQ=/0x0:1800x1200/780x390/data/photo/2022/06/04/629b19acaba3c.jpg"/>
+class App extends Component {
+  clickImage = (e) => {
+    this.setState({
+      ...this.state,
+      img: e.target.src,
+    });
 
-      </div>
-    </div>
-  );
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      img: "https://cdn.mos.cms.futurecdn.net/KNGZRXySsM7quLuNUa6C4h.jpg",
+      isMode: false,
+      listimg: [
+        "https://aasnova.org/wp-content/uploads/2021/09/360-cm-la-silla-cc.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg",
+        "https://cdn.pixabay.com/photo/2015/10/27/08/51/autumn-1008520_960_720.png",
+        "https://img.freepik.com/premium-photo/saturn-planet-with-night-view-rising-sun-realistic-view-solar-system-3d-rendering_508524-249.jpg?w=2000",
+        "https://img.freepik.com/premium-photo/saturn-planet-with-night-view-rising-sun-realistic-view-solar-system-3d-rendering_508524-249.jpg?w=2000",
+        "https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/0/02/OSIRIS_Mars_true_color.jpg",
+      ],
+    };
+  }
+  render() {
+    return (
+      <>
+        <div className={this.state.isMode ? "dark-mode" : "light-mode"}>
+          <div style={{ width: "70%" }}>
+            <div className="container">
+              <span style={{ color: this.state.isMode ? "grey" : "yellow" }}>
+                ☀︎
+              </span>
+              <div className="switch-checkbox">
+                <label className="switch">
+                  <input
+                    type="checkbox"
+                    onChange={() =>
+                      this.setState({
+                        ...this.state,
+                        isMode: !this.state.isMode,
+                      })
+                    }
+                  />
+                  <span className="slider round"> </span>
+                </label>
+              </div>
+              <span style={{ color: this.state.isMode ? "#c96dfd" : "grey" }}>
+                ☽
+              </span>
+            </div>
+            <div>
+              <img className="img" src={this.state.img} alt="img space" />
+              <div className="grid">
+                {this.state.listimg.map((e, i) => (
+                  <div className="parent" key={i}>
+                    <img
+                      className={
+                        !this.state.isMode ? "img-switch" : "img-component"
+                      }
+                      src={e}
+                      onClick={this.clickImage}
+                      // key={i}
+                      alt="img space"
+                    />
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
